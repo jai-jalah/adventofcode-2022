2ff9475e8b24186f6044dc22e74810415fb07850
Jai Jalah <jaijalah@Jais-MacBook-Pro-Work.local>
2022-12-02 11:28:44 +0000

    Complete part 2 of Day 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent of Code 2022
 
 [Day 1 - some elves wanted me to be Excel](https://github.com/jai-jalah/adventofcode-2022/tree/main/day-1)
+[Day 2 - I HATE Rock, Papers & Scissors](https://github.com/jai-jalah/adventofcode-2022/tree/main/day-2)
--- day-2/main.py
+++ day-2/main.py
@@ -14,16 +14,90 @@
   # 5. Sum two scores, store in list.
 # 6. At the end, sum all scores for ans to part 1.
 
+## Part 1
+
+# elf_choices = {
+#   "A" : "Rock",
+#   "B" : "Paper",
+#   "C" : "Scissors"
+# }
+
+# player_choices = {
+#   "X" : "Rock",
+#   "Y" : "Paper",
+#   "Z" : "Scissors"
+# }
+
+# scoring_rules = {
+#   "Rock" : 1,
+#   "Paper" : 2,
+#   "Scissors" : 3,
+#   "Outcomes" : {
+#     "Win" : 6,
+#     "Draw" : 3,
+#     "Loss" : 0
+#   }
+# }
+
+# list_of_scores = []
+
+# def calculate_first_choice_score(player_choice):
+
+#   player_choice = player_choices[player_choice]
+#   player_score = scoring_rules[player_choice]
+ 
+#   return player_score
+
+# def calculate_outcome(elf_choice, player_score):
+#     elf_choice = elf_choices[elf_choice]
+#     elf_score = scoring_rules[elf_choice]
+
+#     if elf_score == player_score:
+#       outcome = "Draw"
+#     elif elf_score == 1 and player_score == 3 or elf_score == 2 and player_score == 1 or elf_score == 3 and player_score == 2: # Ugly hardcoding because I got lost and this finally got me the answer lol
+#       outcome = "Loss"
+#     else:
+#       outcome = "Win"
+
+      
+#     player_score = player_score + scoring_rules["Outcomes"][outcome]
+
+#     list_of_scores.append(player_score)
+
+# with open("input.txt", "r") as input_file:
+#     input = input_file.readlines()
+
+# for round in input:
+#     player_choice = round[2:3]
+#     elf_choice = round[:1]
+
+#     player_score = calculate_first_choice_score(player_choice)
+#     calculate_outcome(elf_choice, player_score)
+
+   
+# part_1_ans = sum(list_of_scores)
+# print(part_1_ans)
+
+
+## Part 2
+
+# 1. X means player needs to lose, Y means player needs to draw, Z means player needs to win.
+# 2. Choose throwing choice, according to outcome needed and elf_choice.
+    # 3. calculate the outcome I need, i.e. draw, win or lose.
+    # 4. calculate the choice I need to make in order to achieve that outcome, depending on what elf chooses. i.e. i need to lose, they choose paper, so i choose rock.
+    # 5. Now add up score of outcome + my choice.
+    # 6. sum at end.
+
 elf_choices = {
   "A" : "Rock",
   "B" : "Paper",
   "C" : "Scissors"
 }
 
-player_choices = {
-  "X" : "Rock",
-  "Y" : "Paper",
-  "Z" : "Scissors"
+outcome_choices = {
+  "X" : "Loss",
+  "Y" : "Draw",
+  "Z" : "Win"
 }
 
 scoring_rules = {
@@ -39,42 +113,49 @@ scoring_rules = {
 
 list_of_scores = []
 
-def calculate_first_choice_score(player_choice):
+def calculate_outcome(outcome_needed):
+    outcome = outcome_choices[outcome_needed]
+    outcome_score = scoring_rules["Outcomes"][outcome]
 
-  player_choice = player_choices[player_choice]
-  player_score = scoring_rules[player_choice]
- 
-  return player_score
+    return outcome, outcome_score
+    
+def calculate_player_choice(outcome, outcome_score, elf_choice):
 
-def calculate_outcome(elf_choice, player_score):
     elf_choice = elf_choices[elf_choice]
-    elf_score = scoring_rules[elf_choice]
+  
+  # This is sooo ugly but I am absolutely not bothered to refactor it for this challenge. Just know that I know it sucks.
 
-    if elf_score == player_score:
-      outcome = "Draw"
-    elif elf_score == 1 and player_score == 3 or elf_score == 2 and player_score == 1 or elf_score == 3 and player_score == 2: # Ugly hardcoding because I got lost and this finally got me the answer lol
-      outcome = "Loss"
-    else:
-      outcome = "Win"
-
-      
-    player_score = player_score + scoring_rules["Outcomes"][outcome]
-
+    if outcome == "Draw":
+      player_choice = elf_choice
+    elif outcome == "Win":
+      if elf_choice == "Rock":
+        player_choice = "Paper"
+      elif elf_choice == "Paper":
+        player_choice = "Scissors"
+      elif elf_choice == "Scissors":
+        player_choice = "Rock"
+    else: # if we need to lose
+      if elf_choice == "Rock":
+        player_choice = "Scissors"
+      elif elf_choice == "Paper":
+        player_choice = "Rock"
+      elif elf_choice == "Scissors":
+        player_choice = "Paper"
+  
+    player_score = scoring_rules[player_choice]
+    player_score = player_score + outcome_score
+    
     list_of_scores.append(player_score)
 
 with open("input.txt", "r") as input_file:
     input = input_file.readlines()
 
 for round in input:
-    player_choice = round[2:3]
+    outcome_needed = round[2:3]
     elf_choice = round[:1]
 
-    player_score = calculate_first_choice_score(player_choice)
-    calculate_outcome(elf_choice, player_score)
+    outcome, outcome_score = calculate_outcome(outcome_needed)
+    calculate_player_choice(outcome_needed, outcome_score, elf_choice)
 
-   
-part_1_ans = sum(list_of_scores)
-print(part_1_ans)
-
-
-
+part_2_ans = sum(list_of_scores)
+print(part_2_ans)
