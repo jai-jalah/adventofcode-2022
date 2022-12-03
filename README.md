acad45b7a4cece6f70573503a731308e4fba90d1
Jai Jalah <jaijalah@Jais-MacBook-Pro-Work.local>
2022-12-03 23:34:59 +0000

    Added day 3 icon, updated README

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Advent of Code 2022
 
+Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. 
+
+All images generateD with AI, by me, on https://nightcafe.studio/. 
+
 [Day 1 - some elves wanted me to be Excel](https://github.com/jai-jalah/adventofcode-2022/tree/main/day-1) <br>
-![Elves using Microsoft Excel](https://github.com/jai-jalah/adventofcode-2022/blob/main/day-1/day-1-icon.jpg)
+![Elves using Microsoft Excel](https://github.com/jai-jalah/adventofcode-2022/blob/main/day-1/day-1-icon.jpg) <br>
+Prompt(s) used: elves using microsoft excel
 <br><br>
 [Day 2 - I HATE Rock, Papers & Scissors](https://github.com/jai-jalah/adventofcode-2022/tree/main/day-2)<br>
-![lots of christmas elves, fighting, big swords - they are playing rock paper scissors](https://github.com/jai-jalah/adventofcode-2022/blob/main/day-2/day-2-icon.jpg)
-<br>
-[Day 3 - Incredibly disorganized elves, in need of better management](https://github.com/jai-jalah/adventofcode-2022/tree/main/day-3)
+![lots of christmas elves, fighting, big swords - they are playing rock paper scissors](https://github.com/jai-jalah/adventofcode-2022/blob/main/day-2/day-2-icon.jpg)<br>
+Prompt(s) used: lots of christmas elves, fighting, big swords, rock paper scissors
+<br><br>
+[Day 3 - Incredibly disorganized elves, in need of better management](https://github.com/jai-jalah/adventofcode-2022/tree/main/day-3)<br>
+![christmas elves, hiking with rucksacks](https://github.com/jai-jalah/adventofcode-2022/blob/main/day-3/day-3-icon.jpg) <br>
+Prompt(s) used: christmas elves, hiding presents in rucksacks
+<br><br>
--- day-3/main.py
+++ day-3/main.py
@@ -8,25 +8,25 @@ Part 1 steps
 5. add priority to answer/total. 
 '''
 
-# part_1_answer = 0
+part_1_answer = 0
 
-# with open("input.txt", "r") as box_of_rucksacks:
-#     rucksacks = box_of_rucksacks.readlines()
+with open("input.txt", "r") as box_of_rucksacks:
+    rucksacks = box_of_rucksacks.readlines()
 
-# for rucksack in rucksacks:
-#     rucksack_first_compartment  = rucksack[:len(rucksack)//2]
-#     rucksack_second_compartment = rucksack[len(rucksack)//2:] # (NB: This method accommodates for odd numbered strings too, just in case.)
+for rucksack in rucksacks:
+    rucksack_first_compartment  = rucksack[:len(rucksack)//2]
+    rucksack_second_compartment = rucksack[len(rucksack)//2:] # (NB: This method accommodates for odd numbered strings too, just in case.)
     
-#     rucksack_first_compartment_sanitised = "".join(set(rucksack_first_compartment)) 
-#     rucksack_second_compartment_sanitised = "".join(set(rucksack_second_compartment)) # Removing duplicate chars in separate rucksacks - this specific methdod will not preserve order of string, so may look strange when printed.
+    rucksack_first_compartment_sanitised = "".join(set(rucksack_first_compartment)) 
+    rucksack_second_compartment_sanitised = "".join(set(rucksack_second_compartment)) # Removing duplicate chars in separate rucksacks - this specific methdod will not preserve order of string, so may look strange when printed.
     
-#     duplicate_char = [char for char in rucksack_first_compartment_sanitised if char in rucksack_second_compartment_sanitised][0]
+    duplicate_char = [char for char in rucksack_first_compartment_sanitised if char in rucksack_second_compartment_sanitised][0]
     
-#     duplicate_char_priority = ord(duplicate_char.lower()) - 96 if (duplicate_char).islower() else ord(duplicate_char.lower()) - 70 # - 96 gets the ord number down to 1 as starting point (i.e. a = 1), - 70 gets it down to 26 (i.e. A = 27)
+    duplicate_char_priority = ord(duplicate_char.lower()) - 96 if (duplicate_char).islower() else ord(duplicate_char.lower()) - 70 # - 96 gets the ord number down to 1 as starting point (i.e. a = 1), - 70 gets it down to 26 (i.e. A = 27)
    
-#     part_1_answer = part_1_answer + duplicate_char_priority
+    part_1_answer = part_1_answer + duplicate_char_priority
 
-# print(f"Part 1 Answer: {part_1_answer}")
+print(f"Part 1 Answer: {part_1_answer}")
 
 '''
 Part 2 steps
