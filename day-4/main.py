9d5f5e43c6effef4a54f2e44db1161fbb955242b
Jai Jalah <jaijalah@Jais-MacBook-Pro-Work.local>
2022-12-04 11:36:13 +0000

    Refactor day 4 repetitive code into fucntion

--- day-4/main.py
+++ day-4/main.py
@@ -11,45 +11,49 @@ Part 1 steps
     
 # 2. if true, add +1 to fully_contained_count. Answer is the total count at the end.
 '''
-assignment_pairs = open("input.txt").read().splitlines()
+def splitter(assignment_pair):
+    split_pairs = assignment_pair.split(",")
+    pair_1, pair_2 = (pair.split("-") for pair in split_pairs)
+  
+    pair_1_sanitised = [eval(num) for num in pair_1]
+    pair_2_sanitised = [eval(num)
+                        for num in pair_2]  # Converting strings to ints.
+  
+    return pair_1_sanitised, pair_2_sanitised
 
+assignment_pairs = open("input.txt").read().splitlines()
+  
 part_1_answer = 0
 
 for assignment_pair in assignment_pairs:
-    split_pairs = assignment_pair.split(",")
-    pair_1, pair_2 = (pair.split("-") for pair in split_pairs)
-
-    pair_1_sanitised = [eval(num) for num in pair_1]
-    pair_2_sanitised = [eval(num) for num in pair_2] # Converting strings to ints.
+    pair_1, pair_2 = splitter(assignment_pair)
   
-    if pair_1_sanitised[0] >= pair_2_sanitised[0] and pair_1_sanitised[1] <= pair_2_sanitised[1]:
-        part_1_answer += 1
-    elif pair_2_sanitised[0] >= pair_1_sanitised[0] and pair_2_sanitised[1] <= pair_1_sanitised[1]:
-        part_1_answer += 1
+    if pair_1[0] >= pair_2[0] and pair_1[
+        1] <= pair_2[1]:
+      part_1_answer += 1
+    elif pair_2[0] >= pair_1[0] and pair_2[
+        1] <= pair_1[1]:
+      part_1_answer += 1
 
 print(f"Part 1 Answer: {part_1_answer}")
-
 '''
 Part 2 steps
 
 # 1. Find overlap of both pairs as ranges.
 '''
-assignment_pairs = open("input.txt").read().splitlines()
-
 part_2_answer = 0
 
 for assignment_pair in assignment_pairs:
-    split_pairs = assignment_pair.split(",")
-    pair_1, pair_2 = (pair.split("-") for pair in split_pairs)
+  pair_1, pair_2 = splitter(assignment_pair)
+  
+  range_overlap = range(
+    max(pair_1[0], pair_2[0]),
+    min(pair_1[-1], pair_2[-1]) + 1
+  )  # Found this on stack overflow lol https://stackoverflow.com/questions/6821156/how-to-find-range-overlap-in-python
 
-    pair_1_sanitised = [eval(num) for num in pair_1]
-    pair_2_sanitised = [eval(num) for num in pair_2] # Converting strings to ints.
-  
-    range_overlap = range(max(pair_1_sanitised[0], pair_2_sanitised[0]), min(pair_1_sanitised[-1], pair_2_sanitised[-1])+1) # Found this on stack overflow lol https://stackoverflow.com/questions/6821156/how-to-find-range-overlap-in-python
-  
-    if len(range_overlap): # will be empty list if no overlap.
-        part_2_answer += 1
+  if len(
+      range_overlap
+  ):  # will be empty list if no overlap, so here we're just checking for any overlap.
+    part_2_answer += 1
 
 print(f"Part 2 Answer: {part_2_answer}")
-
-
