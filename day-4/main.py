c4d6bbb63cbc66f30e3e9f1ea907e158dea7e464
Jai Jalah <jaijalah@Jais-MacBook-Pro-Work.local>
2022-12-04 10:43:52 +0000

    Complete day 4, part 2

--- day-4/main.py
+++ day-4/main.py
@@ -27,4 +27,29 @@ for assignment_pair in assignment_pairs:
     elif pair_2_sanitised[0] >= pair_1_sanitised[0] and pair_2_sanitised[1] <= pair_1_sanitised[1]:
         part_1_answer = part_1_answer + 1
 
-print(part_1_answer)
+print(f"Part 1 Answer: {part_1_answer}")
+
+'''
+Part 2 steps
+
+# 1. Find overlap of both pairs as ranges.
+'''
+assignment_pairs = open("input.txt").read().splitlines()
+
+part_2_answer = 0
+
+for assignment_pair in assignment_pairs:
+    split_pairs = assignment_pair.split(",")
+    pair_1, pair_2 = (pair.split("-") for pair in split_pairs)
+
+    pair_1_sanitised = [eval(num) for num in pair_1]
+    pair_2_sanitised = [eval(num) for num in pair_2] # Converting strings to ints.
+  
+    range_overlap = range(max(pair_1_sanitised[0], pair_2_sanitised[0]), min(pair_1_sanitised[-1], pair_2_sanitised[-1])+1) # Found this on stack overflow lol https://stackoverflow.com/questions/6821156/how-to-find-range-overlap-in-python
+  
+    if len(range_overlap): # will be empty list if no overlap.
+        part_2_answer += 1
+
+print(f"Part 2 Answer: {part_2_answer}")
+
+
